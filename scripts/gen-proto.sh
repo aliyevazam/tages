4363d2ca9488e851d76179ada5678423cbb1b107
Azam Aliyev <alievazam.200@gmail.com>
2023-01-20 09:00:39 +0500

    Added Protofile and generated

---
+++ scripts/gen-proto.sh
@@ -0,0 +1,13 @@
+CURRENT_DIR=$(pwd)
+
+protoc -I /usr/local/include \
+       -I $GOPATH/pkg/mod/github.com/gogo/protobuf@v1.3.2\
+       -I $CURRENT_DIR/protos/ \
+        --gofast_out=plugins=grpc:$CURRENT_DIR/genproto/ \
+        $CURRENT_DIR/protos/*.proto;
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i "" -e "s/,omitempty//g" $CURRENT_DIR/genproto/*.go
+  else
+    sed -i -e "s/,omitempty//g" $CURRENT_DIR/genproto/*.go
+fi
